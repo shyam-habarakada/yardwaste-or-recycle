21a3ca2e00e81d2f50a39e8a1cc3c9b498b41818
Shyam Habarakada <shyam.habarakada@gmail.com>
2013-03-24 21:51:14 -0700

    Additional help for entering the account number

--- YardwasteOrRecycle/YORSettingsTableViewController.m
+++ YardwasteOrRecycle/YORSettingsTableViewController.m
@@ -9,7 +9,8 @@
 #import "YORSettingsTableViewController.h"
 
 @interface YORSettingsTableViewController ()
-
+- (UITableViewCell *)cellForAccountNumberHelp:(UITableView *)tableView;
+- (UITableViewCell *)cellForAccountNumber:(UITableView *)tableView;
 @end
 
 @implementation YORSettingsTableViewController
@@ -37,7 +38,6 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
@@ -49,20 +49,55 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    NSAssert(section == 0, @"settings table should have one section");
+    return 2;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                      reuseIdentifier:CellIdentifier];
-    }
-    cell.textLabel.text = @"Hello";
+    UITableViewCell* cell = nil;
     
+    switch (indexPath.section) {
+        case 0:
+            switch (indexPath.row) {
+                case 0:
+                    cell = [self cellForAccountNumberHelp:tableView];
+                    break;
+                case 1:
+                    cell = [self cellForAccountNumber:tableView];
+                    break;
+                default:
+                    NSAssert(FALSE, @"no such row");
+                    break;
+            }
+            break;
+            
+        default:
+            NSAssert(FALSE, @"no such section");
+            break;
+    }
+    
+    NSAssert(cell, @"could not produce a cell");
     return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    NSAssert(section == 0, @"settings table should have one section");
+    return @"Find the account number in your Seattle Public Utilities bill and type it in for accurate results.";
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0 && indexPath.row == 0)
+    {
+        UITableViewCell* cell = [self cellForAccountNumberHelp:tableView];
+        return cell.contentView.bounds.size.height;
+    }
+    else
+    {
+        return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+    }
 }
 
 /*
@@ -115,6 +150,35 @@
      // Pass the selected object to the new view controller.
      [self.navigationController pushViewController:detailViewController animated:YES];
      */
+}
+
+#pragma mark - Settings table cell helpers
+
+- (UITableViewCell *)cellForAccountNumberHelp:(UITableView *)tableView
+{
+    static NSString *CellIdentifier = @"CellForAccountNumberHelp"; 
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"YORAccountNumberInfoTableViewCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    return cell;
+}
+
+- (UITableViewCell *)cellForAccountNumber:(UITableView *)tableView
+{
+    static NSString *CellIdentifier = @"CellForAccountNumber";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                      reuseIdentifier:CellIdentifier];
+    }
+    cell.textLabel.text = @"Account";
+    cell.detailTextLabel.text = @"2-111111-000000";
+    cell.detailTextLabel.textColor = [UIColor grayColor];
+    return cell;
 }
 
 @end
